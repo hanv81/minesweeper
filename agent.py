192a4a85aa948ab46edb5bd07511f1d6c3ba783b
hanv <hanv@mecorp.vn>
2020-08-19 16:43:27 +0700

    play game with agent

--- agent.py
+++ agent.py
@@ -1,5 +1,8 @@
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import *
+from collections import deque
+
+REPLAY_MEMORY_SIZE = 10000
 
 class Agent:
   def create_model(self, rows, cols):
@@ -15,3 +18,7 @@ class Agent:
   def __init__(self, rows, cols):
     self.actor = self.create_model(rows, cols)
     self.critic = self.create_model(rows, cols)
+    self.replay_memory = deque(maxlen=REPLAY_MEMORY_SIZE)
+
+  def update(self, state, action, reward, next_state, done):
+    self.replay_memory.append((state, action, reward, next_state, done))
--- main.py
+++ main.py
@@ -2,8 +2,24 @@ import gym
 import gym_minesweeper
 from agent import *
 
-rows = 10
-cols = 10
-mines = 10
-env = gym.make('minesweeper-v0', rows=rows, cols=cols, mines=mines)
-agent = Agent(rows, cols)
+ROWS = 10
+COLS = 10
+MINES = 10
+EPISODES = 100
+env = gym.make('minesweeper-v0', rows=ROWS, cols=COLS, mines=MINES)
+agent = Agent(ROWS, COLS)
+
+total_point = 0
+for episode in range(EPISODES):
+    state = env.reset()
+    point = 0
+    while True:
+        action = env.action_space.sample()
+        (next_state, reward, done, info) = env.step(action)
+        point += reward
+        agent.update(state, action, reward, next_state, done)
+        if done:
+            break
+    print('episode ', episode, point)
+    total_point += point
+print('avg point', total_point/episode)
