df164d313d264060d9edd044e4c1a4f51439c40c
hanv <hanv@mecorp.vn>
2020-08-20 22:21:28 +0700

    add train method

--- main.py
+++ main.py
@@ -6,36 +6,44 @@ import numpy as np
 ROWS = 10
 COLS = 10
 MINES = 10
-EPISODES = 1000
-epsilon = 1
+EPISODES = 100
+
 EPSILON_DECAY = 0.99975
 MIN_EPSILON = 0.001
 
 env = gym.make('minesweeper-v0', rows=ROWS, cols=COLS, mines=MINES)
-agent = Agent(ROWS, COLS)
 
-total_point = 0
-for episode in range(EPISODES):
-    state = env.reset()
-    point = 0
-    while True:
-        if np.random.random() > epsilon:
-            action = np.argmax(agent.get_q(state))
-            r = action // ROWS
-            c = action % COLS
-        else:
-            (r,c) = env.action_space.sample()
-            action = r*c
-        (next_state, reward, done, info) = env.step((r,c))
-        point += reward
-        agent.update(state, action, reward, next_state, done)
-        if done:
-            total_point += point
-            env.render()
-            print('episode ', episode+1, point)
-            print('avg point', total_point/(episode+1))
-            break
+def train():
+    epsilon = 1
+    agent = Agent(ROWS, COLS)
 
-    epsilon *= EPSILON_DECAY
-    epsilon = max(MIN_EPSILON, epsilon)
+    total_point = 0
+    for episode in range(EPISODES):
+        state = env.reset()
+        point = 0
+        while True:
+            if np.random.random() > epsilon:
+                action = np.argmax(agent.get_q(state))
+                r = action // ROWS
+                c = action % COLS
+            else:
+                (r,c) = env.action_space.sample()
+                action = r*c
+            (next_state, reward, done, info) = env.step((r,c))
+            point += reward
+            agent.update(state, action, reward, next_state, done)
+            if done:
+                total_point += point
+                env.render()
+                print('episode ', episode+1, point)
+                print('avg point', total_point/(episode+1))
+                break
 
+        epsilon *= EPSILON_DECAY
+        epsilon = max(MIN_EPSILON, epsilon)
+
+def main():
+    train()
+
+if __name__ == "__main__":
+    main()
