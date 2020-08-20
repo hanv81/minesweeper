5d2bd7b95caa2fc761ad96125c86b64e76b3c741
hanv <hanv@mecorp.vn>
2020-08-20 21:57:14 +0700

    update agent

--- agent.py
+++ agent.py
@@ -1,8 +1,14 @@
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import *
 from collections import deque
+import random
+import numpy as np
 
-REPLAY_MEMORY_SIZE = 10000
+REPLAY_MEMORY_SIZE = 50000
+MIN_REPLAY_MEMORY_SIZE = 1000
+MINIBATCH_SIZE = 64
+UPDATE_CRITIC_EVERY = 5
+DISCOUNT = 0.9
 
 class Agent:
   def create_model(self, rows, cols):
@@ -19,6 +25,39 @@ class Agent:
     self.actor = self.create_model(rows, cols)
     self.critic = self.create_model(rows, cols)
     self.replay_memory = deque(maxlen=REPLAY_MEMORY_SIZE)
+    self.critic_update_counter = 0
+
+  def get_q(self, state):
+    return self.actor.predict(np.array(state[None, ...]))
 
   def update(self, state, action, reward, next_state, done):
     self.replay_memory.append((state, action, reward, next_state, done))
+    if len(self.replay_memory) < MIN_REPLAY_MEMORY_SIZE:
+      return
+    batch = random.sample(self.replay_memory, MINIBATCH_SIZE)
+    states = np.array([transition[0] for transition in batch])
+    current_qs_list = self.actor.predict(states)
+
+    next_states = np.array([transition[3] for transition in batch])
+    next_qs_list = self.critic.predict(next_states)
+
+    X = []
+    y = []
+
+    for index, (current_state, action, reward, next_current_state, done) in enumerate(batch):
+      new_q = reward
+      if not done:
+        max_next_q = np.max(next_qs_list[index])
+        new_q += DISCOUNT * max_next_q
+
+      current_qs = current_qs_list[index]
+      current_qs[action] = new_q
+
+      X.append(current_state)
+      y.append(current_qs)
+
+    self.actor.fit(np.array(X), np.array(y), batch_size=MINIBATCH_SIZE, verbose=0)
+    if done:
+      self.critic_update_counter += 1
+      if self.critic_update_counter == UPDATE_CRITIC_EVERY:
+        self.critic.set_weights(self.actor.get_weights())
--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import gym
 import gym_minesweeper
 from agent import *
+import numpy as np
 
 ROWS = 10
 COLS = 10
 MINES = 10
-EPISODES = 100
+EPISODES = 1000
+epsilon = 1
+EPSILON_DECAY = 0.99975
+MIN_EPSILON = 0.001
+
 env = gym.make('minesweeper-v0', rows=ROWS, cols=COLS, mines=MINES)
 agent = Agent(ROWS, COLS)
 
@@ -14,12 +19,23 @@ for episode in range(EPISODES):
     state = env.reset()
     point = 0
     while True:
-        action = env.action_space.sample()
-        (next_state, reward, done, info) = env.step(action)
+        if np.random.random() > epsilon:
+            action = np.argmax(agent.get_q(state))
+            r = action // ROWS
+            c = action % COLS
+        else:
+            (r,c) = env.action_space.sample()
+            action = r*c
+        (next_state, reward, done, info) = env.step((r,c))
         point += reward
         agent.update(state, action, reward, next_state, done)
         if done:
+            total_point += point
+            env.render()
+            print('episode ', episode+1, point)
+            print('avg point', total_point/(episode+1))
             break
-    print('episode ', episode, point)
-    total_point += point
-print('avg point', total_point/episode)
+
+    epsilon *= EPSILON_DECAY
+    epsilon = max(MIN_EPSILON, epsilon)
+
