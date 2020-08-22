cb407104ab2a3ac52bd8b9fd751a78c7539d9df8
hanv <hanv@mecorp.vn>
2020-08-22 20:47:16 +0700

    update plot

--- main.py
+++ main.py
@@ -18,11 +18,8 @@ def train():
     epsilon = 1
     agent = Agent(ROWS, COLS)
 
-    total_point = 0
-    x = []
     y = []
-    max_point = 0
-    max_avg = 0
+    p = []
     for episode in range(EPISODES):
         state = env.reset()
         point = 0
@@ -46,29 +43,22 @@ def train():
             agent.train()
 
         agent.update_critic()
-        if point > max_point:
-            max_point = point
-        total_point += point
-        avg = total_point/(episode+1)
-        if avg > max_avg:
-            max_avg = avg
-        x.append(episode+1)
+
+        p.append(point)
+        avg = sum(p)/(episode+1)
         y.append(avg)
+        
         if (episode + 1) % 10 == 0:
             print("episode %d %d %1.2f"%(episode+1, point, avg))
 
         epsilon *= EPSILON_DECAY
         epsilon = max(MIN_EPSILON, epsilon)
 
-    plot(x, y, 'Train', 'train.png')
-    return max_point, avg, max_avg
+    return p, y
 
 def play_random():
-    total_point = 0
-    x = []
     y = []
-    max = 0
-    max_avg = 0
+    p = []
     for episode in range(EPISODES):
         state = env.reset()
         point = 0
@@ -78,33 +68,34 @@ def play_random():
             next_state, reward, done = env.step(action)
             point += reward
 
-        if point > max:
-            max = point
-        total_point += point
-        avg = total_point/(episode+1)
-        if avg > max_avg:
-            max_avg = avg
-        x.append(episode+1)
+        p.append(point)
+        avg = sum(p)/(episode+1)
         y.append(avg)
+
         if (episode + 1) % 100 == 0:
             print("episode %d %d"%(episode+1, point))
 
-    plot(x, y, 'Random', 'random.png')
-    return max, avg, max_avg
+    return p, y
 
-def plot(x, y, title, filename):
-    plt.figure(figsize=(10,5))
-    plt.scatter(x, y)
+def plot(random_p, random_avg, train_p, train_avg):
+    x = [xi for xi in range(1, EPISODES+1)]
+    plt.figure(figsize=(15,10))
     plt.xlabel('Episode')
     plt.ylabel('Point')
-    plt.title(title)
-    plt.savefig(filename)
+    plt.plot(x, random_avg)
+    plt.plot(x, train_avg)
+    text = 'Max random ' + str(max(random_p)) + '\nMax train ' + str(max(train_p))
+    plt.text(10, 10, text)
+    plt.legend(['Random','Train'])
+    plt.title('Average Point')
+    plt.savefig('minesweeper')
 
 def main():
-    max1, avg1, max_avg1 = play_random()
-    max2, avg2, max_avg2 = train()
-    print('Random %d %1.2f %1.2f'%( max1, avg1, max_avg1))
-    print('Train %d %1.2f %1.2f'%( max2, avg2, max_avg2))
+    p1, avg1 = play_random()
+    p2, avg2 = train()
+    print('Random %d %1.2f %1.2f'%( max(p1), avg1[-1], max(avg1)))
+    print('Train %d %1.2f %1.2f'%( max(p2), avg2[-1], max(avg2)))
+    plot(p1, avg1, p2, avg2)
 
 if __name__ == "__main__":
     main()
