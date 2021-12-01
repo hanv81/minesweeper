135947283f150d1aeb6807acac1e4691625ba9e6
hanv81 <nvha81@gmail.com>
2021-12-01 14:40:32 +0700

    argparse

--- main.py
+++ main.py
@@ -7,18 +7,12 @@ import random
 import matplotlib.pyplot as plt
 import argparse
 
-ROWS = 10
-COLS = 10
-MINES = 10
-
 EPSILON_DECAY = 0.99975
 MIN_EPSILON = 0.001
 
-env = gym.make('minesweeper-v0', rows=ROWS, cols=COLS, mines=MINES)
-
-def trainPG(episodes, cnn=False):
+def trainPG(env, episodes, rows, cols, cnn=False):
     agent = PG()
-    agent.create_model(ROWS, COLS, cnn)
+    agent.create_model(rows, cols, cnn)
     p, avg = agent.train(episodes, env)
     p1, avg1 = play_random(episodes)
     print('------------------ SUMMARY ------------------')
@@ -26,10 +20,10 @@ def trainPG(episodes, cnn=False):
     print('PG:      max %d avg %1.2f max_avg %1.2f'%( max(p), avg[-1], max(avg)))
     plot(avg1, avg)
 
-def train(episodes, cnn=False):
+def train(env, episodes, rows, cols, cnn=False):
     epsilon = 1
     agent = DQN()
-    agent.create_model(ROWS, COLS, cnn)
+    agent.create_model(rows, cols, cnn)
 
     avg = []
     pts = []
@@ -38,17 +32,17 @@ def train(episodes, cnn=False):
         point = 0
         done = False
         clicked_cells = []
-        cells_to_click = [x for x in range(0, ROWS * COLS)]
+        cells_to_click = [x for x in range(0, rows * cols)]
         while not done:
             action = action_policy(agent, state, point, cells_to_click, clicked_cells, epsilon)
-            r = action // COLS
-            c = action % COLS
+            r = action // cols
+            c = action % cols
             next_state, reward, done, info = env.step((r,c))
             agent.step((state, action, reward, next_state, done))
             if reward > 0:
                 point += reward
                 for (r,c) in info:
-                    action = r * COLS + c
+                    action = r * cols + c
                     clicked_cells.append(action)
                     cells_to_click.remove(action)
             state = next_state
@@ -79,31 +73,30 @@ def action_policy(agent, state, point, cells_to_click, clicked_cells, epsilon):
 
     return random.sample(cells_to_click, 1)[0]    # no max action, just random
 
-def play_random(episodes):
-    y = []
-    p = []
+def play_random(env, episodes, rows, cols):
+    avg = []
+    pts = []
     print("Random playing ...")
-    for episode in range(episodes):
+    for _ in range(episodes):
         env.reset()
         point = 0
         done = False
-        cells_to_click = [x for x in range(0, ROWS * COLS)]
+        cells_to_click = [x for x in range(0, rows * cols)]
         while not done:
             action = random.sample(cells_to_click, 1)[0]
-            r = action // COLS
-            c = action % COLS
+            r = action // cols
+            c = action % cols
             _, reward, done, info = env.step((r,c))
             if reward > 0:
                 point += reward
                 for (r,c) in info:
-                    action = r * COLS + c
+                    action = r * cols + c
                     cells_to_click.remove(action)
 
-        p.append(point)
-        avg = sum(p)/(episode+1)
-        y.append(avg)
+        pts.append(point)
+        avg.append(np.mean(pts))
 
-    return p, y
+    return pts, avg
 
 def plot(random_avg, train_avg):
     x = [xi for xi in range(1, len(train_avg)+1)]
@@ -131,23 +124,25 @@ def plot_test(random_avg, dnn_no_heu_avg, dnn_heu_avg, cnn_no_heu_avg, cnn_heu_a
     plt.savefig('test')
 
 def main(args):
-    p1, avg1 = play_random(args.episodes)
-    p2, avg2 = train(args.episodes)
+    env = gym.make('minesweeper-v0', rows=args.rows, cols=args.cols, mines=args.mines)
+    p1, avg1 = play_random(env, args.episodes, args.rows, args.cols)
+    p2, avg2 = train(env, args.episodes, args.rows, args.cols)
     print('------------------ SUMMARY ------------------')
     print('RANDOM:  max %d avg %1.2f max_avg %1.2f'%( max(p1), avg1[-1], max(avg1)))
     print('DQN:     max %d avg %1.2f max_avg %1.2f'%( max(p2), avg2[-1], max(avg2)))
     plot(avg1, avg2)
 
 def test_agent(args):
+    env = gym.make('minesweeper-v0', rows=args.rows, cols=args.cols, mines=args.mines)
     p, avg = play_random(args.episodes)
     agent = DQN()
     agent_cnn = DQN()
-    agent.load_model('./minesweeper/model/model_' + str(ROWS) + '_' + str(COLS) + '_' + str(MINES) + '.h5')
-    agent_cnn.load_model('./minesweeper/model/model_' + str(ROWS) + '_' + str(COLS) + '_' + str(MINES) + '_cnn.h5')
-    p1, avg1, win1 = test(agent, args.episodes)
-    p2, avg2, win2 = test(agent, args.episodes, heuristic=True)
-    p3, avg3, win3 = test(agent_cnn, args.episodes)
-    p4, avg4, win4 = test(agent_cnn, args.episodes, heuristic=True)
+    agent.load_model('./minesweeper/model/model_' + str(args.rows) + '_' + str(args.cols) + '_' + str(args.mines) + '.h5')
+    agent_cnn.load_model('./minesweeper/model/model_' + str(args.rows) + '_' + str(args.cols) + '_' + str(args.mines) + '_cnn.h5')
+    p1, avg1, win1 = test(agent, env, args.episodes, args.rows, args.cols)
+    p2, avg2, win2 = test(agent, env, args.episodes, args.rows, args.cols, heuristic=True)
+    p3, avg3, win3 = test(agent_cnn, env, args.episodes, args.rows, args.cols)
+    p4, avg4, win4 = test(agent_cnn, env, args.episodes, args.rows, args.cols, heuristic=True)
     print('------------------ SUMMARY ------------------')
     print('RANDOM:              max %d avg %1.2f max_avg %1.2f' % (max(p), avg[-1], max(avg)))
     print('DQN:                 max %d avg %1.2f max_avg %1.2f win %d' % (max(p1), avg1[-1], max(avg1), win1))
@@ -156,7 +151,7 @@ def test_agent(args):
     print('DQCNN HEURISTIC:     max %d avg %1.2f max_avg %1.2f win %d' % (max(p4), avg4[-1], max(avg4), win4))
     plot_test(avg, avg1, avg2, avg3, avg4)
 
-def test(agent, episodes, heuristic=False):
+def test(agent, env, episodes, rows, cols, heuristic=False):
     y = []
     p = []
     win = 0
@@ -165,23 +160,23 @@ def test(agent, episodes, heuristic=False):
         point = 0
         done = False
         clicked_cells = []
-        cells_to_click = [x for x in range(0, ROWS * COLS)]
+        cells_to_click = [x for x in range(0, rows * cols)]
         while not done:
             if point == 0: # first cell -> just random
-                action = random.randint(0, ROWS * COLS - 1)
+                action = random.randint(0, rows * cols - 1)
             else:
                 mine_cells = []
                 if heuristic:
-                    for i in range(ROWS):
-                        for j in range(COLS):
+                    for i in range(rows):
+                        for j in range(cols):
                             if state[i,j] > 0:
                                 neibors = []
                                 neibors_flag = []
                                 for r in range(i-1, i+2):
                                     for c in range(j-1, j+2):
-                                        if 0<=r<ROWS and 0<=c<COLS:
+                                        if 0<=r<rows and 0<=c<cols:
                                             if state[r,c] < 0:
-                                                pos = r * COLS + c
+                                                pos = r * cols + c
                                                 if pos in mine_cells:
                                                     neibors_flag.append(pos)
                                                 else:
@@ -190,22 +185,22 @@ def test(agent, episodes, heuristic=False):
                                     for n in neibors:
                                         mine_cells.append(n)
                 qs = agent.predict(state)[0]
-                for cell in range(ROWS*COLS):
+                for cell in range(rows*cols):
                     if cell in clicked_cells or cell in mine_cells:
                         qs[cell] = np.min(qs)
                 if np.max(qs) > np.min(qs):
                     action = np.argmax(qs)
                 else:
                     action = random.sample(cells_to_click, 1)[0]
-            r = action // COLS
-            c = action % COLS
+            r = action // cols
+            c = action % cols
             next_state, reward, done, info = env.step((r,c))
             if reward > 0:
                 if done:
                     win += 1
                 point += reward
                 for (r,c) in info:
-                    action = r * COLS + c
+                    action = r * cols + c
                     clicked_cells.append(action)
                     cells_to_click.remove(action)
             state = next_state
@@ -224,7 +219,10 @@ def parseArgs():
     parser = argparse.ArgumentParser(description = 'An AI Agent for Minesweeper.', formatter_class = argparse.ArgumentDefaultsHelpFormatter)
 
     parser.add_argument('--mode', type = str, default = 'train')
-    parser.add_argument('--episodes', type = int, default = '100')
+    parser.add_argument('--episodes', type = int, default = 100)
+    parser.add_argument('--rows', type = int, default = 10)
+    parser.add_argument('--cols', type = int, default = 10)
+    parser.add_argument('--mines', type = int, default = 10)
     args = parser.parse_known_args()[0]
     return args
 
